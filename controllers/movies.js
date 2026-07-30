78b310b750cbadfb23c2d0feeb216cff52937d15
luisOrtga <ortegaluiskb@gmail.com>
2026-07-30 12:59:44 -0600

    Orchestration async clean code

---
+++ controllers/movies.js
@@ -0,0 +1,59 @@
+import { MovieModel } from "../models/movie"
+
+export class MovieController {
+    static async getAll (req, res) {
+          const { genre } = req.query
+          const movies = await MovieModel.getAll({ genre })
+
+          // qué es lo que renderiza { json, xml, html }
+          res.json(movies)
+    }
+
+    static async getById (req, res) {
+        const { id } = req.params
+        const movie = await MovieModel.getById({ id })
+        if (movie) return res.json(movie)
+
+        // si no se encuentra
+        res.status(404).json({ message: 'Movie not found' })
+    }
+
+    static async create(req, res) {
+        const result = validateMovie(req.body)
+        
+        if (!result.success) {
+            // 422 Unprocessable Entity
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
+        }
+        
+        const newMovie = await MovieModel.create({ input: result.data })
+        
+        res.status(201).json(newMovie)
+    }
+
+    static async detele(req, res) {
+        const { id } = req.params
+        
+        const result = await MovieModel.delete({ id })
+        
+        if (result === false) {
+            return res.status(404).json({ message: 'Movie not found' })
+        }
+        
+        return res.json({ message: 'Movie deleted' })
+    }
+
+    static async update (res, req) {
+        const result = validatePartialMovie(req.body)
+
+        if (!result.success) {
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
+        }
+
+        const { id } = req.params
+  
+        const updatedMovie = await MovieModel.update({ id, input: result.data })
+
+        return res.json(updatedMovie)
+    }
+}
