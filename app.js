57837d277fdf4f99090390372b5f6c63b5d77be3
luisOrtga <ortegaluiskb@gmail.com>
2026-07-30 11:38:02 -0600

    Beging with new api rest node js

---
+++ app.js
@@ -0,0 +1,24 @@
+import express, { json } from 'express' // require -> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middleware/cors.js'
+
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// Todos los recursos que sean MOVIES se identifica con /movies
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
