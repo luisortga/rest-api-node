442e6d1a1c0b74bafa8bb75dbca99b8a6cd6d799
luisOrtga <ortegaluiskb@gmail.com>
2026-08-01 12:46:50 -0600

    Update change database conection with rest api

--- models/local-file-system/movie.js
+++ models/local-file-system/movie.js
@@ -1,5 +1,5 @@
 import { randomUUID } from 'node:crypto'
-import movies from '../movies.json' with { type: 'json' }
+import movies from '../../movies.json' with { type: 'json' }
 
 export class MovieModel {
 
